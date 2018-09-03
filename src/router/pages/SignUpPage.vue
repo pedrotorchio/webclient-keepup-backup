8860b39aaf774c668aaa1f52c54392ac5ebafa23
pedrotorchio <pedrotorchio@gmail.com>
2018-09-03 11:48:40 -0300

    setting up signup form components

--- src/router/pages/SignUpPage.vue
+++ src/router/pages/SignUpPage.vue
@@ -1,10 +1,15 @@
 <script>
+import SignupForm from '@/components/signup/form';
+
 export default {
-  
+  components: {
+    SignupForm
+  }  
 }
 </script>
 <template lang="pug">
-  
+  form
+    signup-form
 </template>
 <style lang="stylus" scoped>
 
