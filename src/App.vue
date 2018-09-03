a68ed59d59cc9c71c3565407677c5473f0dc9a18
pedrotorchio <pedrotorchio@gmail.com>
2018-09-03 11:18:42 -0300

    removing tests

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
 export default {
   name: 'App',
   created() {
-    this.$store.dispatch('test');
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -7,14 +7,5 @@ import { api } from '@/api'; // usar assim
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  actions: {
-    async test() {
-      api.setBaseUrl('http://api.keepup.com.br/api');
-
-      let users = await api.read('users');
-      api.login('pedro@torchiodev.com', '4321');
-          users = await api.read("users");
-
-    }
-  }
+  
 });
