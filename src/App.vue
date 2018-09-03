7dd0d257bb6c3b483cb35a455c25b3b569ce9446
pedrotorchio <pedrotorchio@gmail.com>
2018-09-03 12:49:41 -0300

    full page template

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,22 @@ export default {
   name: 'App',
 }
 </script>
+<style lang="stylus" src='@/styles/helper-classes.styl'></style>
+<style lang="stylus">
+html, body, #app { 
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+body
+  max-width: 100%;
+
+  #app
+    height: auto;
+    min-height: 100%;
+    max-width: 100%;
+
+
+</style>
+
