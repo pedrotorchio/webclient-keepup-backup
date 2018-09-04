61fcb505e81a4945ad8d703d5232f47feb6ca5c2
pedrotorchio <pedrotorchio@gmail.com>
2018-09-03 22:39:45 -0300

    uniformizing error response

--- src/api/interceptors.js
+++ src/api/interceptors.js
@@ -8,22 +8,22 @@ export function responseInterceptor(response) {
 
 export function responseErrorInterceptor(answer) {
   
-  let message = 'Undefined Error';
-  
+  let status = 500;
+  let statusText = 'Internal Error';
+  let data = {
+    error: 'undefined',
+    exception: null,
+    message: 'Undefined Error'
+  };
+
   if (!answer.response) {
     if (answer.message) {
-      message = answer.message;
+      data.message = answer.message;
     }
-
-    throw "Interceptor: " + message;
+  } else {
+    ({ status, statusText, data } = answer.response);
   }
   
-  let {
-    response: {
-      status, statusText, data
-    }
-  } = answer;
-
   throw {
     status,
     statusText,
