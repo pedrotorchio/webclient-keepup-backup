ea750664e063fcc9399ad9be199148fbd8bb4b81
pedrotorchio <pedrotorchio@gmail.com>
2018-09-05 12:29:05 -0300

    initial patients boilerplate

---
+++ src/router/pages/dashboard/patients/index.vue
@@ -0,0 +1,31 @@
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'PatientsPage',
+  computed: {
+    patients() {
+      
+    }
+  },
+  methods: {
+    onUserFetched() {
+      this.$store.dispatch('fetchAllPatients')
+              .then(data => console.dir(data));
+    }
+  },
+  watch: {
+    userData: {
+      immediate: true,
+      deep: true,
+      handler(data) {
+        if (data.id) {
+          this.onUserFetched();
+        }
+      }
+    }
+  }
+}
+</script>
+<template lang="pug" src='./template.pug'></template>
+<style lang="stylus" src='./styles.styl'></style>
