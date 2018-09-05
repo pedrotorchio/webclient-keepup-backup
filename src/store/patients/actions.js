ff4ca6a1e860049369b2f35be619a78618d04c59
pedrotorchio <pedrotorchio@gmail.com>
2018-09-04 22:07:22 -0300

    format

--- src/store/patients/actions.js
+++ src/store/patients/actions.js
@@ -2,7 +2,9 @@ import { api } from "@/api";
 
 export default {
   async fetchAllPatients({ state, getters }) {
+  
     const id = getters.getUserId;
+    
     const patients = await api.get(`users/${id}/patients`);
 
     return patients;
