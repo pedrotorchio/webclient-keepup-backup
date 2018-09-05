7daefe31fd6e34bb3af37b5e8c9000e78326ffab
pedrotorchio <pedrotorchio@gmail.com>
2018-09-05 11:02:40 -0300

    Vue use vuetify and mixins plugins

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -5,12 +6,8 @@ import Vue from 'vue'
 import { api } from '@/api';
 api.setBaseUrl('http://api.keepup.com.br/api');
 
-import Mixins from '@/plugins/Mixins';
-Vue.use(Mixins);
-
-
-
-
+import "@/plugins/vuetify";
+import '@/plugins/Mixins';
 
 import App from "./App";
 import router from "./router";
