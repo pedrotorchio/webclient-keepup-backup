5adaca4f7d779a51d1f8e52607b899b5428a657b
pedrotorchio <pedrotorchio@gmail.com>
2018-09-08 12:06:04 -0300

    extracting list to component

---
+++ src/components/patients/List.vue
@@ -0,0 +1,51 @@
+<script>
+
+export default {
+  props: {
+    patients: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    open(patient) {
+      this.$emit('open', patient);
+    },
+    remove(id) {
+      this.$emit('remove', id);
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.v-list
+  background: transparent !important;
+
+.v-list-tile:last-child
+  margin-bottom: 100px;
+
+</style>
+
+<template lang="pug">
+  v-list(
+    :style=`{
+        height: patients.length * 100 + "px"
+      }`
+  )
+    v-list-tile.padded(
+      v-for="(patient, i) in patients"
+      :key="i"
+      @click.stop="open(patient)"
+    )
+      v-list-tile-avatar
+        img
+
+      v-list-tile-content
+        v-list-tile-title {{ patient.first_name }} {{ patient.last_name }}
+
+      v-list-tile-action(
+        @click.stop="remove(patient.id)"
+      )
+        v-icon move_to_inbox
+
+</template>
--- src/router/pages/dashboard/patients/list/index.vue
+++ src/router/pages/dashboard/patients/list/index.vue
@@ -1,7 +1,11 @@
 <script>
 import { mapState } from 'vuex';
+import List from '@/components/patients/List';
 
 export default {
+  components: {
+    List
+  },
   computed: {
     ...mapState({
       patients: state => state.patients.patients
@@ -23,36 +27,13 @@ export default {
   }
 }
 </script>
-<style lang="stylus" scoped>
-.v-list
-  background: transparent !important;
-
-.v-list-tile:last-child
-  margin-bottom: 100px;
-
-</style>
 
 <template lang="pug">
   div
-    v-list(
-      :style=`{
-          height: patients.length * 100 + "px"
-        }`
+    list(
+      :patients='patients'
+      @open='open'
+      @remove='remove'
     )
-      v-list-tile.padded(
-        v-for="(patient, i) in patients"
-        :key="i"
-        @click.stop="open(patient)"
-      )
-        v-list-tile-avatar
-          img
-
-        v-list-tile-content
-          v-list-tile-title {{ patient.first_name }} {{ patient.last_name }}
-
-        v-list-tile-action(
-          @click.stop="remove(patient.id)"
-        )
-          v-icon move_to_inbox
 
 </template>
