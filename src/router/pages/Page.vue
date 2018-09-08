bc4d6a21187514bded623e72608d23ca5cbf17fc
pedrotorchio <pedrotorchio@gmail.com>
2018-09-08 20:55:09 -0300

    creating Route parent component

--- src/router/pages/Page.vue
+++ src/router/pages/Page.vue
@@ -1,22 +1,10 @@
 <script>
 import { mapState } from 'vuex';
+import Route from './Route';
 
 export default {
+  extends: Route,
   name: 'Page',
-  computed: {
-    ...mapState({
-      userData: state => state.auth.user
-    }),
-    title() {
-      if (this.$route.meta && this.$route.meta.title) {
-        const title = this.$route.meta.title;
-
-        return title(this.$route, this);
-      }
-
-      return false;
-    }
-  },
   created() {
     this.$store.dispatch('checkSession');
   }
---
+++ src/router/pages/Route.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'Route',
+  computed: {
+    ...mapState({
+      userData: state => state.auth.user
+    }),
+    title() {
+      if (this.$route.meta && this.$route.meta.title) {
+        const title = this.$route.meta.title;
+
+        return title(this.$route, this);
+      }
+
+      return false;
+    }
+  }
+}
+</script>
