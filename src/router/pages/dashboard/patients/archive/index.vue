0d6bc84d82c7c2dff93e8df39a21e3ec43f5fe75
pedrotorchio <pedrotorchio@gmail.com>
2018-09-08 13:14:33 -0300

    route component

--- src/router/pages/dashboard/patients/archive/index.vue
+++ src/router/pages/dashboard/patients/archive/index.vue
@@ -1,3 +1,51 @@
+<script>
+import { mapState } from 'vuex';
+import List from '@/components/patients/List';
+
+export default {
+  name: 'Arquivo',
+  components: {
+    List
+  },
+  data: () => ({
+    archive: []
+  }),
+  methods: {
+    open(patient) {},
+    restore(id) {
+      
+    }
+  },
+  computed: {
+    ...mapState({
+      userData: state => state.patients.patients
+    })
+  },
+  watch: {
+    userData: {
+      immediate: true,
+      deep: true,
+      handler(data) {
+        if (data)
+          this.$store.dispatch('fetchArchivedData')
+              .then(patients => {
+                patients.forEach( patient => {
+                  this.archive.push(patient);
+                });
+              });
+      }
+    }
+  }
+}
+</script>
+
 <template lang="pug">
-  h1 ARQUIVO
+div
+  list(
+      :patients='archive'
+      icon='how_to_vote'
+
+      @open='open'
+      @action='restore'
+    )  
 </template>
