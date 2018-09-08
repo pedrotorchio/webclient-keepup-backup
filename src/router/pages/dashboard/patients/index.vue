3954c5331c31f18eb5dfab306744f827401d8aaa
pedrotorchio <pedrotorchio@gmail.com>
2018-09-08 11:06:07 -0300

    removing unecessary patients properties

--- src/router/pages/dashboard/patients/index.vue
+++ src/router/pages/dashboard/patients/index.vue
@@ -7,32 +7,11 @@ import { debounce } from 'debounce';
 window.debounce = debounce;
 export default {
   name: 'PatientsPage',
-  components: {
-    List,
-    PatientForm
-  },
   props: {
     patientId: {
       type: Number | Boolean,
       default: false
     }
-  },
-  computed: {
-    ...mapState({
-      patients: state => state.patients.patients
-    }),
-    patient() {
-      let patient = false;
-      
-      if (this.patientId) {
-        patient = this.patients.find(patient => patient.id == this.patientId)
-      }
-
-      return patient || false;
-    },
-    isList() {
-      return !Boolean(this.patientId);
-    },
   },
   methods: {
     async submitProcedure(data) {
