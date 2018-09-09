1b2567aee72b368f455f3ca9dc6058c89c3124c9
pedrotorchio <pedrotorchio@gmail.com>
2018-09-09 11:24:44 -0300

    extracting page to inner view

--- src/router/pages/dashboard/routines/index.vue
+++ src/router/pages/dashboard/routines/index.vue
@@ -1,17 +1,11 @@
 <script>
 import Patient from '@/router/pages/dashboard/patients/mixins/Patient.mixin';
 export default {
-  mixins: [ Patient ],
-  data: () => ({
-    routines: []
-  }),
-  created() {
-    this.$store.dispatch('fetchPatientRoutines')
-        
-  }
+  name: 'Routines'
 }
 </script>
 
 <template lang="pug">
-  div {{ routines }}
+  div
+    router-view
 </template>
---
+++ src/router/pages/dashboard/routines/list/index.vue
@@ -0,0 +1,20 @@
+<script>
+import Patient from '@/router/pages/dashboard/patients/mixins/Patient.mixin';
+export default {
+  mixins: [ Patient ],
+  data: () => ({
+    routines: []
+  }),
+  created() {
+    this.$store.dispatch('fetchPatientRoutines', this.patientId)
+        .then( routines => {
+          routines.forEach( routine => this.routines.push(routine) );
+        });
+        
+  }
+}
+</script>
+
+<template lang="pug">
+  div {{routines}}
+</template>
