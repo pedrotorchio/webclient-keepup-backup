66fc94d53b4155a9e72e3e2d840dd331a903f9b2
pedrotorchio <pedrotorchio@gmail.com>
2018-09-09 11:06:06 -0300

    routines initial

---
+++ src/router/pages/dashboard/routines/index.vue
@@ -0,0 +1,17 @@
+<script>
+import Patient from '@/router/pages/dashboard/patients/mixins/Patient.mixin';
+export default {
+  mixins: [ Patient ],
+  data: () => ({
+    routines: []
+  }),
+  created() {
+    this.$store.dispatch('fetchPatientRoutines')
+        
+  }
+}
+</script>
+
+<template lang="pug">
+  div {{ routines }}
+</template>
