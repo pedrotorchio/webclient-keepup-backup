9050eeb0a6b06cdde6f736e61bff8ca7d3cae265
pedrotorchio <pedrotorchio@gmail.com>
2018-09-09 11:24:34 -0300

    fixed fetching id

--- src/store/routines/actions.js
+++ src/store/routines/actions.js
@@ -2,8 +2,8 @@ import { api } from '@/api';
 
 export default {
   async fetchPatientRoutines({}, id) {
-    let routines = await api.get(`patients/2/routines`);
-
+    let routines = await api.get(`patients/${id}/routines`);
+    
     return routines;
   }
 }
