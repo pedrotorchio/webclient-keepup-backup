cf0f4bf257d771299dfefe99b3b5b40f23118796
pedrotorchio <pedrotorchio@gmail.com>
2018-09-11 17:21:14 -0300

    including routine floating buttons

--- src/router/pages/dashboard/routines/index.vue
+++ src/router/pages/dashboard/routines/index.vue
@@ -1,11 +1,38 @@
 <script>
 import Patient from '@/router/pages/dashboard/patients/mixins/Patient.mixin';
+import Route from '@/router/pages/Route';
+
 export default {
-  name: 'Routines'
+  extends: Route,
+  name: 'Routines',
+  data: () => ({
+    
+  }),
+  watch: {
+    pacientId: {
+      immediate: true,
+      handler(id) {
+        this.rootActions.push({ 
+          tip: 'Lista de Rotinas', 
+          to: { name: 'RoutinesList', params: { pacientId: id } }, 
+          color: 'info', 
+          icon: 'list' 
+        });
+        this.rootActions.push({ 
+          tip: 'Adicionar Rotina', 
+          to: { name: 'RoutinesNew', params: { pacientId: id } },
+          color: 'primary', 
+          icon: 'add' 
+        });
+      }
+    }
+  }
 }
 </script>
 
 <template lang="pug">
   div
-    router-view
+    router-view(
+      :actions.sync="viewActions"
+    )
 </template>
