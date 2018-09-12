c56a5c26ab18b322a3640f154281584fef679700
pedrotorchio <pedrotorchio@gmail.com>
2018-09-12 15:22:16 -0300

    routines mixin

---
+++ src/router/pages/dashboard/routines/mixins/Routine.mixin.vue
@@ -0,0 +1,56 @@
+<script>
+
+export default {
+  props: {
+    routineId: {
+      type: Number | Boolean,
+      default: false
+    },
+    routineTitle: {
+      type: Number | Boolean,
+      default: false
+    },
+  },
+  data: () => ({
+    routine__: null
+  }),
+  computed: {
+    routine: {
+      get() {
+        
+        if (!this.routineId) {
+          this.routine__ = null; 
+        }
+        
+        return this.routine__;  
+      },
+      set(data) {
+        this.routine__ = data;
+      }      
+    }
+  },
+  methods: {
+    watchRoutine(data) {
+    }
+  },
+  watch: {
+    routine: {
+      immediate: true,
+      deep: true,
+      handler(data) {
+        this.watchRoutine(data);
+      }
+    },
+    routineId: {
+      immediate: true,
+      handler(id) {
+        if (id) {
+          
+          this.$store.dispatch('fetchRoutine', id)
+              .then( routine => this.routine = routine );
+        }
+      }
+    }
+  },
+}
+</script>
