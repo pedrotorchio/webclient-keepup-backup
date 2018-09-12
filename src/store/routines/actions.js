55341e014468fc97be59d2447314a8019f390a7f
pedrotorchio <pedrotorchio@gmail.com>
2018-09-12 15:22:31 -0300

    routine actions

--- src/store/routines/actions.js
+++ src/store/routines/actions.js
@@ -5,5 +5,28 @@ export default {
     let routines = await api.get(`patients/${id}/routines`);
     
     return routines;
+  },
+  async fetchRoutine({}, id) {
+    let routines = await api.get(`routines/${id}`);
+    
+    return routines;
+  },
+  async createRoutine({}, { patientId, data }) {
+    let routines = await api.post(`patients/${patientId}/routines`, data);
+    
+    return routines;
+  },
+  async updateRoutine({ state }, data) {
+    const { id } = data;
+
+    const response = await api.put(`routines/${id}`, data);
+    
+    return response;
+  },
+  async deleteRoutine({ state }, id) {
+    
+    await api.delete(`routines/${id}`);
+
+    return true;
   }
 }
