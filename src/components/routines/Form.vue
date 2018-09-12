9f4f07cbd7783c9f9de7a99a27095fc43f95d7e6
pedrotorchio <pedrotorchio@gmail.com>
2018-09-11 23:16:42 -0300

    routines form component

---
+++ src/components/routines/Form.vue
@@ -0,0 +1,124 @@
+<script>
+import moment from 'moment';
+
+const formData = () => ({
+      title: '',
+      comment: '',
+      date: ''
+});
+
+export default {
+  name: 'Form',
+  props: {
+    patient: {
+      type: Object | Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    isValid: false,
+    form: formData(),
+    updated_at: '',
+    dateModal: false
+  }),
+  methods: {
+    dateChanged() {
+      this.dateModal = false;
+    },
+    changed(key) {
+      
+      const { date } = this.form;
+
+      if (date) {
+
+        const data = { ...this.form };
+        this.$emit('change', data);
+      }
+    },
+  },
+  computed: {
+    textFields() {
+      return [
+        [ 'title', 'Título'],
+        [ 'comment' , 'Comentário'],
+      ]
+    },
+    dateFields() {
+      return [
+        [ 'date' , 'Data'],
+      ]
+    },
+    now() {
+      return moment().format('YYYY-MM-DD');
+    },
+  },
+  watch: {
+    patient: {
+      immediate: true,
+      deep: true,
+      handler(data) {
+        if (data) {
+          this.form = {...this.patient};
+        } else {
+          this.form = formData();
+        }
+      }
+    }
+  }
+}
+</script>
+<template lang="pug">
+v-form(
+    v-model='isValid'
+  )
+  //- v-date-picker( 
+  //-   v-model="form.date" 
+  //-   :show-current="now"
+  //-   @input="changed(date)"
+  //-   )
+  v-dialog(
+    ref="dialog"
+    v-model="dateModal"
+    lazy
+    full-width
+    width="290px" )
+      
+    v-text-field(
+      slot="activator"
+      v-model="form.date"
+      label="Data"
+      prepend-icon="event" )
+      
+    v-date-picker( 
+      v-model="form.date" 
+      @input="dateChanged"
+      scrollable )
+        
+//-   v-text-field( 
+//-     v-for="([key, label], i) in textFields"
+//-     v-model='form[key]'
+//-     :class="key"
+//-     :label='label'
+//-     @input="changed(key)"
+//-     required
+//-   )
+</template>
+<style lang="stylus" scoped>
+.slider
+  margin: 2em;
+
+form
+  display: flex;
+  flex-wrap: wrap;
+
+.v-input
+  flex: 1 1 auto;
+  min-width: 10em;
+
+.email
+  flex-basis: 100%;
+.last_name 
+  flex-grow: 3;
+  flex-basis: 20em;
+
+</style>
