4100521ce22a0ad993df26f4bd2c74320587012f
pedrotorchio <pedrotorchio@gmail.com>
2018-09-12 15:20:19 -0300

    routines form

--- src/router/pages/dashboard/routines/form/index.vue
+++ src/router/pages/dashboard/routines/form/index.vue
@@ -1,18 +1,48 @@
 <script>
+
 import Route from '@/router/pages/Route';
 import FormView from '@/components/generic/form/FormView.mixin';
 import Patient from '@/router/pages/dashboard/patients/mixins/Patient.mixin';
+import Routine from '@/router/pages/dashboard/routines/mixins/Routine.mixin';
 import RoutineForm from '@/components/routines/Form';
+import { createRoutine, updateRoutine } from './procedures';
 
 export default {
   extends: Route,
   components: { RoutineForm },
-  mixins: [ Patient, FormView ],
+  mixins: [ Patient, Routine, FormView ],
   name: 'Form', 
+
+  methods: {
+    ////// submit@FormView is called whenever form emits @change
+    ///// submit calls submitProcedure
+
+    async submitProcedure(data) {
+
+      let routine;
+      
+      if (this.$route.name === 'RoutinesNew') {
+        
+        routine = await createRoutine.bind(this)({ patientId: this.patientId, data});
+
+      } else {
+        
+        data.id = this.routine.id;
+        routine = await updateRoutine.bind(this)(data);
+
+      }
+
+      return routine;
+    },
+  },
+  
 }
 </script>
 
 <template lang="pug">
 div
-  routine-form
+  routine-form(
+    :model="routine"
+    @change='submit'
+  )
 </template>
