a0eb0118c07f6f632b330bf3327b489e1e2098e3
pedrotorchio <pedrotorchio@gmail.com>
2018-09-11 23:19:29 -0300

    routines form view

---
+++ src/router/pages/dashboard/routines/form/index.vue
@@ -0,0 +1,18 @@
+<script>
+import Route from '@/router/pages/Route';
+import FormView from '@/components/generic/form/FormView.mixin';
+import Patient from '@/router/pages/dashboard/patients/mixins/Patient.mixin';
+import RoutineForm from '@/components/routines/Form';
+
+export default {
+  extends: Route,
+  components: { RoutineForm },
+  mixins: [ Patient, FormView ],
+  name: 'Form', 
+}
+</script>
+
+<template lang="pug">
+div
+  routine-form
+</template>
