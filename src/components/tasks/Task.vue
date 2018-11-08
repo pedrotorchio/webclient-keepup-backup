104275da2932dfe337e0d2951eac197a1a2e3167
pedrotorchio <pedrotorchio@gmail.com>
2018-11-08 10:09:03 -0300

    creating task component

---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    model: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({
+    selected: false
+  })
+}
+</script>
+<template lang="pug">
+  div.task(
+    :class = `{ selected }`
+  )
+</template>
