2971596797d0e91bc0f82e6007b90162d03e1e4a
pedrotorchio <pedrotorchio@gmail.com>
2018-11-11 15:05:42 -0300

    fixing canvas height getter

--- src/router/pages/dashboard/routines/view/Visualization.mixin.vue
+++ src/router/pages/dashboard/routines/view/Visualization.mixin.vue
@@ -1,5 +1,6 @@
 <script>
 import {  Scales } from '@/visualization/tasks';
+import { parse, format } from '@/visualization/utils/time';
 
 export default {
   data: () => ({
@@ -8,7 +9,8 @@ export default {
       duration: null,
       independency: null,
       category: null,
-      type: null
+      type: null,
+      height: null
     },
     categories: [],
     timeExtent: [],
@@ -25,14 +27,41 @@ export default {
         this.scales.type
       )
     },
+    hasHeightScalesComputed() {
+      return Boolean(
+        this.scales.height && this.scales.category && this.scales.type
+      );
+    },
     hours() {
+      if (this.timeExtent.length == 0)
+        return 0;
+
       return this.timeExtent[1].getHours() - this.timeExtent[0].getHours();
-    }
+    },
   },
   methods: {
+    format,
+    getCanvasHeight() {
+      if (!this.$el || !this.$refs.xAxis)
+        return 0;
+
+      return this.$el.clientHeight - this.$refs.xAxis.clientHeight;
+    },
+    getCanvasWidth() {
+      if (!this.$refs.canvas)
+        return 0;
+      
+      return this.$refs.canvas.clientWidth;
+    },
+    getHourTickWidth(hour) {
+      if (hour === this.hours + 1)
+        return 'auto';
+      
+
+      return `${this.scales.duration(60)}px`;
+    },  
     async createVisualization() {
 
-      const canvas = this.$refs.canvas; 
       const scales = new Scales(this.tasks);
 
       this.categories = scales.categories;
@@ -40,24 +69,27 @@ export default {
       this.titles     = scales.titles;
       this.padding    = scales.padding;
 
-      scales.setCanvasHeight(canvas.clientHeight);
+      scales.setCanvasHeight(this.getCanvasHeight());
       this.scales.height = scales.getHeightScale();
       this.scales.category = scales.getCategoryScale();
       this.scales.type = scales.getTypeScale();
 
+      this.scales.independency = scales.getIndependencyScale();
+
       setTimeout(() => {
         
-        this.scales.independency = scales.getIndependencyScale();
-        
-        scales.setCanvasWidth(canvas.clientWidth);
+        scales.setCanvasWidth(this.getCanvasWidth());
         this.scales.moment = scales.getMomentScale();
         this.scales.duration = scales.getDurationScale();
-
 
       }, 100);
 
     },
+    getNextHourTick(offset = 0) {
+      const [ start ] = this.timeExtent;
 
+      return parse(start.getHours() + offset, 'h');
+    }
   }
 }
 </script>
