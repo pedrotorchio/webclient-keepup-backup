7f40ccb5cc51ed691bae151c39502d033702ea76
pedrotorchio <pedrotorchio@gmail.com>
2018-11-11 11:25:49 -0300

    async width calculation

--- src/router/pages/dashboard/routines/view/Visualization.mixin.vue
+++ src/router/pages/dashboard/routines/view/Visualization.mixin.vue
@@ -7,25 +7,54 @@ export default {
       moment: null,
       duration: null,
       independency: null,
-      category: null
-    }
-
+      category: null,
+      type: null
+    },
+    categories: [],
+    timeExtent: [],
+    titles: [],
+    padding: 0
   }),
+  computed: {
+    hasScalesComputed() {
+      return Boolean(
+        this.scales.moment && 
+        this.scales.duration && 
+        this.scales.independency && 
+        this.scales.category &&
+        this.scales.type
+      )
+    }
+  },
   methods: {
     async createVisualization() {
 
-      const scales = new Scales(this.tasks, this.$el.clientWidth, this.$el.clientHeight);
+      const canvas = this.$refs.canvas; 
+      const scales = new Scales(this.tasks);
 
-      this.scales.moment = scales.getMomentScale();
-      this.scales.duration = scales.getDurationScale();
-      this.scales.independency = scales.getIndependencyScale();
-      this.scales.category = scales.getCategoryScale();
+      this.categories = scales.categories;
+      this.timeExtent = scales.timeExtent;
+      this.titles     = scales.titles;
+      this.padding    = scales.padding;
+
+      scales.setCanvasHeight(canvas.clientHeight);
       this.scales.height = scales.getHeightScale();
+      this.scales.category = scales.getCategoryScale();
+      this.scales.type = scales.getTypeScale();
 
-      // this.scales.height = categoryScale(this.tasks, this.canvasVerticalRange);
+      setTimeout(() => {
+        
+        this.scales.independency = scales.getIndependencyScale();
+        
+        scales.setCanvasWidth(canvas.clientWidth);
+        this.scales.moment = scales.getMomentScale();
+        this.scales.duration = scales.getDurationScale();
 
 
-    }
+      }, 100);
+
+    },
+
   }
 }
 </script>
