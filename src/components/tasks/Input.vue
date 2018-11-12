59fbbd0f0d396a00cd9c591034d8d12803b5fac3
pedrotorchio <pedrotorchio@gmail.com>
2018-11-11 23:01:41 -0300

    tooltip input

---
+++ src/components/tasks/Input.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  props: {
+    value: {
+      required: true
+    },
+    editable: {
+      type: Boolean,
+      default: false
+    },
+    color: {
+      type: String,
+      default: "primary"
+    },
+    icon: {
+      type: String,
+      required: false
+    },
+    append: {
+      type: String,
+      default: ''
+    }
+  },
+  
+}
+</script>
+<template lang="pug">
+  div.input
+    v-icon( v-if = "icon" :color="color") {{icon}}
+    span.text( v-if = "!editable" ) {{ value + append }}
+    input.text( v-else
+      :value = "value"
+      @input = "$emit('update:value', value)"
+    )
+    span.append( v-if = "editable && append") {{ append }}
+</template>
+
+<style lang="stylus" scoped>
+
+.input 
+  font-size 18px;
+.text
+  white-space nowrap
+  margin .5em 0
+  
+.v-icon
+  margin-right .5em !important;
+
+</style>
