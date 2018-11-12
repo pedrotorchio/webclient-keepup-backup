a80ec6d91d7fdedc2fcc914f46fec3638daf2202
pedrotorchio <pedrotorchio@gmail.com>
2018-11-11 23:26:26 -0300

    selection

--- src/components/routines/Stats.mixin.vue
+++ src/components/routines/Stats.mixin.vue
@@ -1,16 +1,23 @@
 <script>
 
 export default {
-
   computed: {
     totalDuration() {
       let duration = 0;
-      this.tasks.map( task => duration += task.duration );
+
+      this.collection.map( task => duration += task.duration );
 
       return duration;
-    }
-  },
-
-  
+    },
+    selectedElements() {
+      return this.$refs.tasks.filter( task => task.selected )
+    },
+    selection() {
+      return this.selectedElements.map( el => el.model );
+    },
+    collection() {
+      return this.selection.length > 0 ? this.selection : this.tasks;
+    },
+  },  
 }
 </script>
