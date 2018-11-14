2a636f66a9fea406695c3d41b2a2f14db3cfe358
pedrotorchio <pedrotorchio@gmail.com>
2018-11-14 17:16:04 -0300

    user data watcher

---
+++ src/router/mixins/UserDataWatcher.vue
@@ -0,0 +1,22 @@
+<script>
+import { mapState } from 'vuex';
+export default {
+  computed: {
+    ...mapState({
+      userData: state => state.auth.user
+    })
+  },
+  methods: {
+    onUserDataChange(data) {}
+  },
+  watch: {
+    userData: {
+      immediate: true,
+      deep: true,
+      handler(data) {
+       this.onUserDataChange(data); 
+      }
+    }
+  },
+}
+</script>
