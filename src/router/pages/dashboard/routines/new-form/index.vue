1c193a75b17aa2190e5f1320b4a4cadcdcd32d25
pedrotorchio <pedrotorchio@gmail.com>
2018-11-20 20:09:51 -0300

    new form page component

---
+++ src/router/pages/dashboard/routines/new-form/index.vue
@@ -0,0 +1,14 @@
+<script>
+import Route from '@/router/pages/Route';
+import RoutineRoute from '@/router/mixins/RoutineRoute';
+
+export default {
+  extends: Route,
+  mixins: [ RoutineRoute ]
+
+}
+
+</script>
+<template lang="pug">
+  div
+</template>
