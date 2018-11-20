4e965dbacfbb8c6e16f5a1e26664508a260657b0
pedrotorchio <pedrotorchio@gmail.com>
2018-11-19 22:55:56 -0300

    tasksform mixin

---
+++ src/router/pages/dashboard/routines/view/TasksForm.mixin.vue
@@ -0,0 +1,18 @@
+<script>
+import TasksRoute from '@/router/mixins/TasksRoute';
+import { mapActions } from 'vuex';
+
+export default {
+  extend: TasksRoute,
+  methods: {
+    ...mapActions({
+      updateFormsTask: 'updateFormsTask'
+    }),
+    toggleForm(id) {
+      const index = this.forms.findIndex( form => form.id === id );
+      
+      console.log(this.forms[index].visibility);
+    }
+  }
+}
+</script>
