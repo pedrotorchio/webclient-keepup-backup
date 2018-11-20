3aa6caadd0de147cfab611e9b5cf00340b90a0ae
pedrotorchio <pedrotorchio@gmail.com>
2018-11-19 22:34:28 -0300

    fetching tasks from forms

--- src/router/mixins/TasksRoute.vue
+++ src/router/mixins/TasksRoute.vue
@@ -7,26 +7,32 @@ export default {
   extends: RoutineRoute,
   methods: {
     ...mapActions([
-      'fetchRoutineTasks'
+      'fetchRoutineTasksForms',
+      'fetchTasksForm',
     ]),
     onRoutineLoaded() {
      this.loadTasks(); 
     },
     async loadTasks() {
-      this.tasks = await this.fetchRoutineTasks(this.routineId);
+      this.forms = await this.fetchRoutineTasksForms(this.routineId);
       
-      this.tasks = this.tasks.map( task => {
-        const timestring = `${task.time} ${this.routine.date}`;
-        const moment = parseDatetime(timestring, true);
+      const formFetcher = this.forms.map( async form => await this.fetchTasksForm(form.id) );
+      const forms = await Promise.all(formFetcher);
+      this.tasks = forms.flatMap( form => form.tasks )
+                        .map( task => {
+
+          const timestring = `${task.time} ${this.routine.date}`;
+          const moment = parseDatetime(timestring, true);
+          
+          task.start = moment.toDate();
+          task.end   = moment.add(task.duration, 'm').toDate();
+          
+          return task;
+          
+        }).sort( ( task1, task2 ) => task1.start - task2.start );
         
-        task.start = moment.toDate();
-        task.end   = moment.add(task.duration, 'm').toDate();
-        
-        return task;
-        
-      }).sort( ( task1, task2 ) => task1.start - task2.start );
-      
-      this.tasksLoaded();
+        // this.tasksLoaded();
+
     },
     tasksLoaded() {}
   }
