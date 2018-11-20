38aaf5bbf0051a4722ce097a7a5e94e5e7d714bb
pedrotorchio <pedrotorchio@gmail.com>
2018-11-19 23:13:25 -0300

    toggle form visibility method

--- src/router/pages/dashboard/routines/view/TasksForm.mixin.vue
+++ src/router/pages/dashboard/routines/view/TasksForm.mixin.vue
@@ -4,12 +4,15 @@ import { mapActions } from 'vuex';
 export default {
   methods: {
     ...mapActions({
-      updateFormsTask: 'updateFormsTask'
+      updateTasksForm: 'updateTasksForm'
     }),
-    toggleForm(id) {
-      const index = this.forms.findIndex( form => form.id === id );
-      
-      console.log(this.forms[index].visibility);
+    toggleForm(formId) {
+      const { id, visibility } = this.forms.find( form => form.id === formId );    
+
+      this.updateTasksForm({
+        id,
+        visibility: !visibility
+      });
     }
   }
 }
