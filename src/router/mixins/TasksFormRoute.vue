7327b5810244fc37d5a0d0e55f182c2636f0ecc7
pedrotorchio <pedrotorchio@gmail.com>
2018-11-20 22:26:58 -0300

    tasks form route

---
+++ src/router/mixins/TasksFormRoute.vue
@@ -0,0 +1,29 @@
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  props: {
+    tasksFormId: {
+      type: Number
+    }
+  },
+  data: () => ({
+    tasksForm: null
+  }),
+  methods: {
+    ...mapActions({
+      fetchTasksForm: 'fetchTasksForm'
+    }),
+    async loadTasksForm() {
+      this.tasksForm = await this.fetchTasksForm( this.patientId );
+
+      this.onTasksFormLoaded();
+    },
+    onTasksFormLoaded() {},
+  },
+  created() {
+    if (this.tasksFormId)
+      this.loadTasksForm();
+  }
+}
+</script>
