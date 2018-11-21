58a19e3739c33a5f4d76f3646d4d788c47e14408
pedrotorchio <pedrotorchio@gmail.com>
2018-11-20 22:27:23 -0300

    new tasks form page

--- src/router/pages/dashboard/routines/new-form/index.vue
+++ src/router/pages/dashboard/routines/new-form/index.vue
@@ -1,14 +1,55 @@
 <script>
 import Route from '@/router/pages/Route';
 import RoutineRoute from '@/router/mixins/RoutineRoute';
+import TasksFormRoute from '@/router/mixins/TasksFormRoute';
+import NewForm from '@/components/tasks-form/Form';
+import FormView from '@/components/generic/form/FormView.mixin';
+import { mapActions } from 'vuex';
+
 
 export default {
   extends: Route,
-  mixins: [ RoutineRoute ]
+  mixins: [ TasksFormRoute, RoutineRoute, FormView ],
+  components: { NewForm },
+  methods: {
+    ...mapActions({
+      updateTasksForm: 'updateTasksForm',
+      createTasksForm: 'createTasksForm'
+    }),
+    async submitProcedure(data) {
+      
+      let form;
 
+      if (this.tasksForm) {
+
+        data.id = this.tasksForm.id;
+        form = await this.updateTasksForm(data);
+
+      } else {
+        const routineId = this.routineId;
+        
+        form = await this.createTasksForm({
+          routineId,
+          data  
+        });
+
+        this.$router.push({
+          name: 'FormEdit',
+          params: {
+            formId: form.id
+          }
+        });
+      }
+
+      return form;
+    }
+  }
 }
 
 </script>
 <template lang="pug">
-  div
+  new-form(
+    :model='tasksForm'
+    @change='submit'
+  )
 </template>
