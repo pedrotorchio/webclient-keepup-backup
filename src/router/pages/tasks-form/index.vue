b20284918767d7511fbc8db6448c7a706f7de54a
pedrotorchio <pedrotorchio@gmail.com>
2018-11-22 10:21:32 -0300

    public form

---
+++ src/router/pages/tasks-form/index.vue
@@ -0,0 +1,17 @@
+<script>
+import TasksFormRoute from '@/router/mixins/TasksFormRoute';
+import Route from '@/router/pages/Route';
+
+export default {
+  extends: Route,
+  mixins: [ TasksFormRoute ],
+  props: {
+    formUid: {
+      type: Number
+    }
+  }
+}
+</script>
+<template lang="pug" src="./template.pug"></template>
+<style lang="stylus" src="./styles.styl" scoped></style>
+
