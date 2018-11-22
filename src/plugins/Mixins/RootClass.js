811ed926220ff5a6f1e39f0070ec5045b268742c
pedrotorchio <pedrotorchio@gmail.com>
2018-11-22 11:18:30 -0300

    preventing root class error

--- src/plugins/Mixins/RootClass.js
+++ src/plugins/Mixins/RootClass.js
@@ -1,8 +1,9 @@
 export default {
   mounted() {
-    
-    if (this.$el && this.$el.classList)
-      this.$el.classList.add('root'); 
+    if (!this.$el || !this.$el.classList)
+      return;
+
+    this.$el.classList.add('root'); 
     
     if (this.$options.name)
       this.$el.classList.add(`${this.$options.name}`);
