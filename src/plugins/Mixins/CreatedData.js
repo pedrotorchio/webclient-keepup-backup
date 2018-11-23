8ad3169a323385ae804904a7d9c177ad087c357f
pedrotorchio <pedrotorchio@gmail.com>
2018-11-22 23:50:00 -0300

    improving created data plugin

--- src/plugins/Mixins/CreatedData.js
+++ src/plugins/Mixins/CreatedData.js
@@ -4,6 +4,6 @@ export default {
   }),
   created() {
     const { data__, ...data } = this.$data;
-    this.data__ = Object.assign({}, data);
+    this.data__ = JSON.parse(JSON.stringify(data));
   }
 };
