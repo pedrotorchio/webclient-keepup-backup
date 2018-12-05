b761913409710501be2a4936565ad33ed1876691
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-05 18:38:20 -0300

    caregiver view

---
+++ src/components/caregivers/CaregiverView.vue
@@ -0,0 +1,39 @@
+<script>
+import { mapActions } from 'vuex';
+import FormView from '@/components/generic/form/FormView.mixin';
+
+export default {
+    extends: FormView,
+    data: () => ({
+        caregiverFormShown: false
+    }),
+    methods: {
+        ...mapActions({
+            storeCreateCaregiver: 'createCaregiver',
+            storeDeleteCaregiver: 'deleteCaregiver',
+        }),
+        createCaregiver(data) {
+            this.submit(data);
+        },
+        toggleCaregiverFormShown() {
+            this.caregiverFormShown = !this.caregiverFormShown;
+        },
+        async submitProcedure(data) {
+
+            data.id = this.form.id; 
+            const caregiver = await this.storeCreateCaregiver(data);
+
+            if (this.model.caregivers)
+                this.model.caregivers.push(data);
+
+            this.toggleCaregiverFormShown();
+        },
+        async deleteCaregiver(id) {
+            // await this.deleteCaregiver(id);
+
+            if (this.model.caregivers)
+                this.model.caregivers = this.model.caregivers.filter( cg => cg.id !== id );
+        }
+    }
+}
+</script>
