710c8cb3820fbeb9cce3a9348043cb8f059e4541
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-07 11:28:27 -0300

    setting base folder environment

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  ROOT_API: '"http://api.timediary.com.br/api"'
+  ROOT_API: '"http://api.timediary.com.br/api"',
+  BASE_FOLDER: '"/"',
 })
