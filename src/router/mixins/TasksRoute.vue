a67dfa493ef4b652912e7c8be34ce42ab33d86e6
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-18 13:14:10 -0300

    @extractTasks: task parser, limit to midnight and sort

--- src/router/mixins/TasksRoute.vue
+++ src/router/mixins/TasksRoute.vue
@@ -22,12 +22,13 @@ export default {
 
       this.forms = this.forms.map( form => { // for each form
         const parser = taskParser(this.routine);
-        
-        const parseAndLimit = task => 
-        form.tasks = form.tasks.map( parser );
+        const parseAndLimit = task => taskChopper(parser(task));
+        const dateSorter = ( task1, task2 ) => task1.start.getTime() - task2.start.getTime();
+
+        form.tasks = form.tasks.map( parseAndLimit ).sort( dateSorter );
 
         return form;
-      })
+      });
       
       this.extractTasks();
       this.tasksLoaded();
