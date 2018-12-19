63759b30b56140afb07b251327e872fa6202b977
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-18 22:47:23 -0300

    mousemove event

--- src/components/tasks/HoverHandler.vue
+++ src/components/tasks/HoverHandler.vue
@@ -4,6 +4,21 @@ export default {
     hover(event) {
       this.$refs.tooltip.show();
     },
+    move(event) {
+      if (this.editable)
+        return;
+        
+      const { target, pageX, pageY } = event;
+      const { left: offsetLeft, top: offsetTop, height, width } = target.getBoundingClientRect();
+
+      let left = pageX - offsetLeft + 10;
+          left = left > width - 10 ? width  : left;
+      
+      let top = pageY - offsetTop + 10;
+          top = top > height - 10 ? height : top;
+
+      this.$refs.tooltip.setPosition(left, top);
+    },
     hoverOut(event) {
       if (!this.editable)
         this.$refs.tooltip.hide();
