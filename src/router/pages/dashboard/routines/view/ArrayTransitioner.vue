870294121026913092014f8361781b8bdf878516
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-20 21:15:36 -0300

    base list transition component

---
+++ src/router/pages/dashboard/routines/view/ArrayTransitioner.vue
@@ -0,0 +1,32 @@
+<script>
+let enterCount = 0;
+
+export default {
+    props: {
+        delay: {
+            type: Number,
+            default: 100
+        },
+        duration: {
+            type: Number,
+            default: .2
+        }
+    },
+    computed: {
+        arraySize() {
+            return 0;
+        }
+    },
+    methods: {
+        entered() {
+            enterCount++;
+            
+            if (enterCount >= this.arraySize) {
+                
+                this.$emit('done-showing');
+            }
+                
+        }
+    }
+}
+</script>
