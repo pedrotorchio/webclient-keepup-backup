01a40c61d964748adc57ecca8ae04fe11680cc78
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-20 23:32:56 -0300

    Columns component

---
+++ src/router/pages/dashboard/routines/view/Columns.vue
@@ -0,0 +1,17 @@
+<script>
+import xAxis from './xAxis';
+export default {
+    extends: xAxis,
+}
+</script>
+<template lang="pug">
+    transition-group#columns( appear tag = "div" 
+        v-on = "$listeners" v-bind = "$attrs" 
+        :css = "false"
+        @after-enter = "entered"
+        @enter = "fadeIn"
+    )
+        p.tick(  v-for = "( hour, i ) in hourCount" :key = "hour"
+            :style = "{ width: `${tickWidth}px` }"
+            :data-index = "i"   )
+</template>
