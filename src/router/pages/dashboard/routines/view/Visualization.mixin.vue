68ee34c756908113767f4783171c374b70da2ed3
Pedro Torchio <pedrotorchio@gmail.com>
2018-12-20 21:16:35 -0300

    simplifying visualization mixin

--- src/router/pages/dashboard/routines/view/Visualization.mixin.vue
+++ src/router/pages/dashboard/routines/view/Visualization.mixin.vue
@@ -10,30 +10,16 @@ export default {
     doneScaling: false
   }),
   computed: {
+    hourTickWidth() {
+      if (!this.scales)
+        return 0;
+
+      const durationScale = this.scales.getDurationScale();
+
+      return durationScale(60);
+    },
     hasTasks() {
       return Boolean(this.scales) && this.scales.hasTasks();
-    },
-
-    hours() {
-      if (this.scales === null)
-        return 0;
-
-      const timeExtent = this.scales.timeExtent; 
-
-      if (timeExtent.length == 0)
-        return 0;
-
-      const [ startTime, endTime ] = timeExtent;
-
-      const isMultipleDays = !isSameDay(endTime, startTime);
-
-      const start = startTime.getHours();
-      const end = isMultipleDays ? 24 : endTime.getHours();
-
-      return  end - start;
-    },
-    taskCount() {
-      return this.tasks.length;
     },
     rowHeight() {
       if (!this.scales)
@@ -59,58 +45,30 @@ export default {
   },
   methods: {
     format,
-    rowStyle() {
-      return { 
-        height: `${this.rowHeight}px`, 
-        padding: `${this.padding}px`  
-      }
-    },
     tasksLoaded() {
-      
       this.hasLoaded = true;
-      
       this.scales = new Scales(this.allTasks);
-
-      if (this.hasMounted)
-        this.createVisualization();
-
     },
     getCanvasWidth() {
       
-      if (!this.hasMounted || !this.$refs.canvas)
-        return 0;
+      if (!this.$refs.canvas)
+        throw "CANVAS not mounted";
       
-      return this.$refs.canvas.clientWidth;
+      return this.$refs.canvas.$el.clientWidth;
     },
-    getHourTickWidth(hour) {
-      
-      if (hour === this.hours + 1)
-        return 'auto';
-      
-      const durationScale = this.scales.getDurationScale();
-
-      return `${durationScale(60)}px`;
-    },  
+    
     async createVisualization() {
 
       setTimeout(() => {
-        
         this.scales.setCanvasWidth(this.getCanvasWidth());
         this.doneScaling = true;
 
-      }, 1000);
+      }, 0);
 
     },
-    getNextHourTick(offset = 0) {
-      const [ start, end ] = this.scales.timeExtent;
-
-      return parse(start.getHours() + offset, 'h');
-    }
   },
   mounted() {
     this.hasMounted = true;
-    if (this.hasLoaded)
-      this.createVisualization();
   }
 }
 </script>
