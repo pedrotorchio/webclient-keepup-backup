4155c8f74427b178c07a68c9cf6dab29491e86b0
Pedro Torchio <pedrotorchio@gmail.com>
2019-02-15 12:06:58 -0300

    insirting local is loading and generic loading abstraction

--- src/plugins/Mixins/SetLoading.js
+++ src/plugins/Mixins/SetLoading.js
@@ -1,10 +1,21 @@
 import { mapActions, mapState } from 'vuex'
 
 export default {
+    data: {
+        localIsLoading___: false
+    },
     computed: {
         ...mapState({
             gIsLoading: state => state.system.isLoading
-        })
+        }),
+        isLoading: {
+            get() {
+                return this.gIsLoading || this.localIsLoading
+            },
+            set(value) {
+                this.localIsLoading___ = value
+            }            
+        }
     },
     methods: {
         ...mapActions({
