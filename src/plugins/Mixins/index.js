f47c5afbdb4df02cccc177605d817367db7230ad
Pedro Torchio <pedrotorchio@gmail.com>
2019-06-09 15:47:13 +1000

    VUE constant capability plugin

--- src/plugins/Mixins/index.js
+++ src/plugins/Mixins/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import HookLogger from './HookLogger';
+import VueConstants from 'vue-constants';
 import RootClass from './RootClass';
 import CreatedData from './CreatedData';
 import SetLoading from './SetLoading';
 import SetMessage from './SetMessage';
 
+Vue.use(VueConstants);
 Vue.use({
   install(Vue, options) {
     // Vue.mixin(HookLogger);
