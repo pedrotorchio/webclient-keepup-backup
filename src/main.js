6b1ff07bff7739f053f18a7ddecd12afefe554fd
Pedro Torchio <pedrotorchio@gmail.com>
2020-06-08 17:35:33 +1000

    configuring vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '@babel/polyfill'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+Vue.config.productionTip = false
 
 import { api } from '@/api';
 api.setBaseUrl(process.env.ROOT_API);
@@ -11,13 +12,12 @@ import '@/plugins/Mixins';
 import '@/plugins/Directives';
 import '@/plugins/Components';
 
-import App from "./App";
+import vuetify from './plugins/vuetify';
 import router from "./router";
 import store from "./store";
-import vuetify from './plugins/vuetify';
 
 
-Vue.config.productionTip = false
+import App from "./App";
 /* eslint-disable no-new */
 new Vue({
   name: 'Root',
